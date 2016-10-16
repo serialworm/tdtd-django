609ecd376d66ac27032cf6dd3f7b7307f8e0e277
Eric Beringer <eberinger@cirruspath.com>
2016-10-15 17:27:37 -0700

    Getting started

---
+++ mysite/private/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.template import loader
+
+
+def index(request):
+    template = loader.get_template('private/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def login(request):
+    template = loader.get_template('private/login.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
